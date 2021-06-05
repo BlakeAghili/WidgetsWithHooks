42d04ef20fd67ecb8e89e59263887508c2cd8e06
BlakeAghili <peopleofksmo@gmail.com>
2021-06-05 09:18:46 -0400

    Routing Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import DropDown from "./components/DropDown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 const items = [
   { title: "What is Life", content: "I do not know" },
@@ -22,11 +23,51 @@ const options = [
   { label: "A Blue Color", value: "blue" },
 ];
 
+const showAccordion = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <DropDown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />;
+  }
+};
+
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
+  const [selected, setSelected] = useState(options[1]);
   return (
     <div>
-      <Translate />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <DropDown
+          label="Select A Color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
